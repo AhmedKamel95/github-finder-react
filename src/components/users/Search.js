8c85e0aca18463541379b0d820c25c3acda2d7c5
ahmedkamel095 <ahmedkamel095@gmail.com>
2019-12-03 23:10:00 +0200

    Refactored app to functional based

---
+++ src/components/users/Search.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const { useState } = React;
+
+const Search = ({ showClear, clearUsers, searchUsers, setAlert }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (text === "") {
+      setAlert("Please enter something", "light");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
+
+  const onChange = e => setText(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search Users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
